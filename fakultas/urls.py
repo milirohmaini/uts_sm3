f4eb0c1a3cf8b16f84dac44d596e2db9c3311e6e
hasan <2225210016@untirta.ac.id>
2022-10-04 15:46:21 +0700

    project pertama

---
+++ fakultas/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('fkip/', views.fkip, name='fkip'),
+    path('faperta/', views.faperta, name='faperta'),
+    path('feb/', views.feb, name='feb'),
+    path('fh/', views.fh, name='fh'),
+    path('fisip/', views.fisip, name='fisip'),
+    path('fk/', views.fk, name='fk'),
+    path('ft/', views.fkip, name='ft'),
+    path('pascasarjana/', views.pascasarjana, name='pascasarjana'),
+    path('selayangpandang/', views.selayangpandang, name='selayangpandang'),
+    path('visidanmisi/', views.visidanmisi, name='visidanmisi'),
+    path('strukturorganisasi/', views.strukturorganisasi, name='strukturorganisasi'),
+    path('maknalambang/', views.maknalambang, name='maknalambang'),
+    
+]
---
+++ fakultas/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+
+
+# Create your views here.
+def index(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
+def fkip(request):
+    template = loader.get_template('fkip.html')
+    return HttpResponse(template.render())
+def feb(request):
+    template = loader.get_template('feb.html')
+    return HttpResponse(template.render())
+def fh(request):
+    template = loader.get_template('fh.html')
+    return HttpResponse(template.render())
+def fisip(request):
+    template = loader.get_template('fisip.html')
+    return HttpResponse(template.render())
+def fk(request):
+    template = loader.get_template('fk.html')
+    return HttpResponse(template.render())
+def ft(request):
+    template = loader.get_template('ft.html')
+    return HttpResponse(template.render())
+def pascasarjana(request):
+    template = loader.get_template('pascasarjana.html')
+    return HttpResponse(template.render())
+def faperta(request):
+    template = loader.get_template('faperta.html')
+    return HttpResponse(template.render())
+def selayangpandang(request):
+    template = loader.get_template('selayangpandang.html')
+    return HttpResponse(template.render())
+def visidanmisi(request):
+    template = loader.get_template('visidanmisi.html')
+    return HttpResponse(template.render())
+def strukturorganisasi(request):
+    template = loader.get_template('strukturorganisasi.html')
+    return HttpResponse(template.render())
+def maknalambang(request):
+    template = loader.get_template('maknalambang.html')
+    return HttpResponse(template.render())
+

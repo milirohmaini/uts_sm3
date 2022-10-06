87da93f58079da5d8c679fd1de4eff3a14172c99
hasan <2225210016@untirta.ac.id>
2022-10-06 22:27:05 +0700

    project 11

--- fakultas/models.py
+++ fakultas/models.py
@@ -2,7 +2,34 @@ from django.db import models
 
 # Create your models here.
 
-class MahasiswaFkip(models.Model):
+class DosenFaperta(models.Model):
+    nip = models.CharField(max_length=50)
+    nama = models.CharField(max_length=100)
+    ttl = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    fakultas = models.CharField(max_length=100)
+    prodi = models.CharField(max_length=100)
+    alamat = models.CharField(max_length=300)
+    foto = models.CharField(max_length=300, null=True)
+
+
+    def _str_(self):
+        return "{}".format(self.nama)
+
+class StaffFaperta(models.Model):
+    nip = models.CharField(max_length=50)
+    nama = models.CharField(max_length=100)
+    ttl = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    unit = models.CharField(max_length=100)
+    alamat = models.CharField(max_length=300)
+    foto = models.CharField(max_length=300, null=True)
+
+
+    def _str_(self):
+        return "{}".format(self.nama)
+
+class MahasiswaFaperta(models.Model):
     nim = models.CharField(max_length=50)
     nama = models.CharField(max_length=100)
     ttl = models.CharField(max_length=50)
@@ -16,7 +43,174 @@ class MahasiswaFkip(models.Model):
     def _str_(self):
         return "{}".format(self.nama)
 
+class DosenFeb(models.Model):
+    nip = models.CharField(max_length=50)
+    nama = models.CharField(max_length=100)
+    ttl = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    fakultas = models.CharField(max_length=100)
+    prodi = models.CharField(max_length=100)
+    alamat = models.CharField(max_length=300)
+    foto = models.CharField(max_length=300, null=True)
+
+
+    def _str_(self):
+        return "{}".format(self.nama)
+
+class StaffFeb(models.Model):
+    nip = models.CharField(max_length=50)
+    nama = models.CharField(max_length=100)
+    ttl = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    unit = models.CharField(max_length=100)
+    alamat = models.CharField(max_length=300)
+    foto = models.CharField(max_length=300, null=True)
+
+
+    def _str_(self):
+        return "{}".format(self.nama)
+
+class MahasiswaFeb(models.Model):
+    nim = models.CharField(max_length=50)
+    nama = models.CharField(max_length=100)
+    ttl = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    fakultas = models.CharField(max_length=100)
+    prodi = models.CharField(max_length=100)
+    alamat = models.CharField(max_length=300)
+    foto = models.CharField(max_length=300, null=True)
+
+
+    def _str_(self):
+        return "{}".format(self.nama)
+
+
+class DosenFh(models.Model):
+    nip = models.CharField(max_length=50)
+    nama = models.CharField(max_length=100)
+    ttl = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    fakultas = models.CharField(max_length=100)
+    prodi = models.CharField(max_length=100)
+    alamat = models.CharField(max_length=300)
+    foto = models.CharField(max_length=300, null=True)
+
+
+    def _str_(self):
+        return "{}".format(self.nama)
+
+class StaffFh(models.Model):
+    nip = models.CharField(max_length=50)
+    nama = models.CharField(max_length=100)
+    ttl = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    unit = models.CharField(max_length=100)
+    alamat = models.CharField(max_length=300)
+    foto = models.CharField(max_length=300, null=True)
+
+
+    def _str_(self):
+        return "{}".format(self.nama)
+
+class MahasiswaFh(models.Model):
+    nim = models.CharField(max_length=50)
+    nama = models.CharField(max_length=100)
+    ttl = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    fakultas = models.CharField(max_length=100)
+    prodi = models.CharField(max_length=100)
+    alamat = models.CharField(max_length=300)
+    foto = models.CharField(max_length=300, null=True)
+
+
+    def _str_(self):
+        return "{}".format(self.nama)
+
+class DosenFisip(models.Model):
+    nip = models.CharField(max_length=50)
+    nama = models.CharField(max_length=100)
+    ttl = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    fakultas = models.CharField(max_length=100)
+    prodi = models.CharField(max_length=100)
+    alamat = models.CharField(max_length=300)
+    foto = models.CharField(max_length=300, null=True)
+
+
+    def _str_(self):
+        return "{}".format(self.nama)
+
+class StaffFisip(models.Model):
+    nip = models.CharField(max_length=50)
+    nama = models.CharField(max_length=100)
+    ttl = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    unit = models.CharField(max_length=100)
+    alamat = models.CharField(max_length=300)
+    foto = models.CharField(max_length=300, null=True)
+
+
+    def _str_(self):
+        return "{}".format(self.nama)
+
+class MahasiswaFisip(models.Model):
+    nim = models.CharField(max_length=50)
+    nama = models.CharField(max_length=100)
+    ttl = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    fakultas = models.CharField(max_length=100)
+    prodi = models.CharField(max_length=100)
+    alamat = models.CharField(max_length=300)
+    foto = models.CharField(max_length=300, null=True)
+
+
+    def _str_(self):
+        return "{}".format(self.nama)
+
+class DosenFk(models.Model):
+    nip = models.CharField(max_length=50)
+    nama = models.CharField(max_length=100)
+    ttl = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    fakultas = models.CharField(max_length=100)
+    prodi = models.CharField(max_length=100)
+    alamat = models.CharField(max_length=300)
+    foto = models.CharField(max_length=300, null=True)
+
+
+    def _str_(self):
+        return "{}".format(self.nama)
+
+class StaffFk(models.Model):
+    nip = models.CharField(max_length=50)
+    nama = models.CharField(max_length=100)
+    ttl = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    unit = models.CharField(max_length=100)
+    alamat = models.CharField(max_length=300)
+    foto = models.CharField(max_length=300, null=True)
+
+
+    def _str_(self):
+        return "{}".format(self.nama)
+
+class MahasiswaFk(models.Model):
+    nim = models.CharField(max_length=50)
+    nama = models.CharField(max_length=100)
+    ttl = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    fakultas = models.CharField(max_length=100)
+    prodi = models.CharField(max_length=100)
+    alamat = models.CharField(max_length=300)
+    foto = models.CharField(max_length=300, null=True)
+
+
+    def _str_(self):
+        return "{}".format(self.nama)
+
+
 class DosenFkip(models.Model):
+    nomor = models.CharField(max_length=300, null=True)
     nip = models.CharField(max_length=50)
     nama = models.CharField(max_length=100)
     ttl = models.CharField(max_length=50)
@@ -31,6 +225,7 @@ class DosenFkip(models.Model):
         return "{}".format(self.nama)
 
 class StaffFkip(models.Model):
+    nomor = models.CharField(max_length=300, null=True)
     nip = models.CharField(max_length=50)
     nama = models.CharField(max_length=100)
     ttl = models.CharField(max_length=50)
@@ -42,3 +237,103 @@ class StaffFkip(models.Model):
 
     def _str_(self):
         return "{}".format(self.nama)
+
+class MahasiswaFkip(models.Model):
+    nomor = models.CharField(max_length=300, null=True)
+    nim = models.CharField(max_length=50)
+    nama = models.CharField(max_length=100)
+    ttl = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    fakultas = models.CharField(max_length=100)
+    prodi = models.CharField(max_length=100)
+    alamat = models.CharField(max_length=300)
+    foto = models.CharField(max_length=300, null=True)
+
+
+    def _str_(self):
+        return "{}".format(self.nama)
+
+class DosenFt(models.Model):
+    nip = models.CharField(max_length=50)
+    nama = models.CharField(max_length=100)
+    ttl = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    fakultas = models.CharField(max_length=100)
+    prodi = models.CharField(max_length=100)
+    alamat = models.CharField(max_length=300)
+    foto = models.CharField(max_length=300, null=True)
+
+
+    def _str_(self):
+        return "{}".format(self.nama)
+
+class StaffFt(models.Model):
+    nip = models.CharField(max_length=50)
+    nama = models.CharField(max_length=100)
+    ttl = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    unit = models.CharField(max_length=100)
+    alamat = models.CharField(max_length=300)
+    foto = models.CharField(max_length=300, null=True)
+
+
+    def _str_(self):
+        return "{}".format(self.nama)
+
+class MahasiswaFt(models.Model):
+    nim = models.CharField(max_length=50)
+    nama = models.CharField(max_length=100)
+    ttl = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    fakultas = models.CharField(max_length=100)
+    prodi = models.CharField(max_length=100)
+    alamat = models.CharField(max_length=300)
+    foto = models.CharField(max_length=300, null=True)
+
+
+    def _str_(self):
+        return "{}".format(self.nama)
+
+class DosenPascasarjana(models.Model):
+    nip = models.CharField(max_length=50)
+    nama = models.CharField(max_length=100)
+    ttl = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    fakultas = models.CharField(max_length=100)
+    prodi = models.CharField(max_length=100)
+    alamat = models.CharField(max_length=300)
+    foto = models.CharField(max_length=300, null=True)
+
+
+    def _str_(self):
+        return "{}".format(self.nama)
+
+class StaffPascasarjana(models.Model):
+    nip = models.CharField(max_length=50)
+    nama = models.CharField(max_length=100)
+    ttl = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    unit = models.CharField(max_length=100)
+    alamat = models.CharField(max_length=300)
+    foto = models.CharField(max_length=300, null=True)
+
+
+    def _str_(self):
+        return "{}".format(self.nama)
+
+class MahasiswaPascasarjana(models.Model):
+    nim = models.CharField(max_length=50)
+    nama = models.CharField(max_length=100)
+    ttl = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    fakultas = models.CharField(max_length=100)
+    prodi = models.CharField(max_length=100)
+    alamat = models.CharField(max_length=300)
+    foto = models.CharField(max_length=300, null=True)
+
+
+    def _str_(self):
+        return "{}".format(self.nama)
+
+
+
